7578d8190d6e4600282c31670ec6c54413aa70be
Robert Sarkozi <sarkozi.robert@gmail.com>
2020-08-23 22:24:21 +0300

    Added new programs to autostart

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-killall dwm_bar.sh
+
+killall -q dwm_bar.sh
 /home/robcsi/git/dwm/dwm-bar/dwm_bar.sh &
 compton &
+nm-applet &
+artha &
+orage &
+dunst &
+mate-power-manager &
+mate-volume-control-applet &
+killall -q nitrogen
+nitrogen --random --set-zoom-fill /home/robcsi/wallpapers/
