42f600fa0fdb67f6adde8d07410a24abfb8088b9
Robert Sarkozi <sarkozi.robert@gmail.com>
2020-12-01 00:53:57 +0200

    Refactored autostart; added dwm-status, the rust project

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -2,12 +2,13 @@
 
 #/home/robcsi/git/dwm/dwm-bar/dwm_bar.sh &
 #killall -q dwm_status.sh ||
-/home/robcsi/git/dwm/dwm-status/dwm_status.py &
+#/home/robcsi/git/dwm/dwm-status/dwm_status.py &
+
+pkill dwm-status || ~/.local/bin/dwm-status ~/.dwm-status.json &
 compton &
 nm-applet &
 artha &
-orage &
 dunst &
-mate-power-manager &
-mate-volume-control-applet &
-killall -q nitrogen || nitrogen --random --set-zoom-fill /home/robcsi/wallpapers/ &
+xfce4-power-manager &
+pa-applet &
+blueman-applet &
